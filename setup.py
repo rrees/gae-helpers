7d2340cd5787b603d3c082609dfa83d766b92e8f
Robert Rees <shudderfix@gmail.com>
2017-06-25 21:46:23 +0100

    Adds Python package configuration

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+	name = "gae-helpers",
+	packages = ["gae-helpers"],
+	version = "0.0.1",
+	description = "A collection of utilities for GAE applications",
+	author = "Robert Rees",
+	author_email = "rrees-pypi@shudderfix.com",
+	url = "https://github.com/rrees/gae-helpers",
+	download_url = "https://github.com/rrees/gae-helpers/archive/0.0.1.tar.gz",
+	keywords = ['google app engine'],
+	classifiers = [],
+)
